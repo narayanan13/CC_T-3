ba88d0a4acd649922b49c2bd2100989eb224f9a2
Sonali-123PSG <swamy07sona@gmail.com>
2023-07-03 14:24:56 +0530

    second commit

---
+++ ArraySum.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class ArraySum {
+    static int arr[] = { 12, 3, 4, 15, 30 };
+
+    static int sum() {
+        int sum = 0;
+        int i;
+        for (i = 0; i < arr.length; i++)
+            sum += arr[i];
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Sum of given array is " + sum());
+    }
+}
